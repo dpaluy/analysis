2fc84a7e3c623bffc77d016ebd0d23a1fc2551f0
David P <david@paluy.org>
2011-11-25 14:14:43 +0200

    Charts added

--- app/helpers/values_helper.rb
+++ app/helpers/values_helper.rb
@@ -1,2 +1,12 @@
 module ValuesHelper
+
+#  def values_series(quote)
+#    start_time = quote.values.first.timestamp
+#    end_time =  start_time + 1.minute
+#    #quote.values.second.timestamp
+#    (start_time..end_time).map { |d| 
+#      v = @quote.values.where("timestamp = ?", d)
+#      [v.timestamp, v.value]
+#     }.inspect
+#  end
 end
